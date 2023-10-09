6aed4b5e7688a1b97bb1dfdfa574fad286179b26
adyatand <adyatandagar234@gmail.com>
2023-10-09 17:34:31 -0400

    Commit A

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Your Personal Finance Manager</h1>
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
       </header>
     </div>
   );

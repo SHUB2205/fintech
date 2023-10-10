0d3bd874f9b5a4b4e0c40df4ac98fd68f4a14541
adyatand <adyatandagar234@gmail.com>
2023-10-10 14:16:24 -0400

    Commit harder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Fuck me</h1>
+        <h2>HARDER!!!!!!!!!!</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

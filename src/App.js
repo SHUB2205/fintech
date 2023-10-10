e7dc79bbc354b44a29dffbc89f8815aa7c384fab
SHUB2205 <parasharshubhankar22@gmail.com>
2023-10-10 14:39:15 -0400

    Commit nav

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Fuck me</h1>
-        <h2>HARDER!!!!!!!!!!</h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Personal Finance Management Software</h1>
+        <h2>Don't spend your hard earned money without keeping a record.</h2>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Fintech</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Transactions</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" aria-disabled="true">Guide</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         
       </header>
     </div>

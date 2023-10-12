8234c7e7509519bec36cf0ac521fc124170e18fd
SHUB2205 <parasharshubhankar22@gmail.com>
2023-10-12 17:49:21 -0400

    Login ready

--- src/components/form.js
+++ src/components/form.js
@@ -25,7 +25,35 @@ function Form(){
                             </div>
                             <button type='submit' class='btn'>Login</button>
                             <div class='login-register'>
-                                <p>Don't have an account<a href='#' class='rgister-link'>Register</a></p>
+                                <p>Don't have an account<a href='#' class='register-link'>Register</a></p>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class='form-box Register'>
+                        <h2>Register</h2>
+                        <form action='#'>
+                        <div class='input-box'>
+                                <span class="icon"></span>
+                                <input type="text" required/>
+                                <label>Username</label>
+                            </div>
+                            <div class='input-box'>
+                                <span class="icon"></span>
+                                <input type="email" required/>
+                                <label>Email</label>
+                            </div>
+                            <div class='input-box'>
+                                <span class="icon"></span>
+                                <input type="password" required/>
+                                <label>Password</label>
+                            </div>
+                            <div class='remember'>
+                                <label><input type='checkbox'/>Agree to terms and conditions.</label>
+                            </div>
+                            <button type='submit' class='btn'>Register</button>
+                            <div class='login-register'>
+                                <p>Already have an account.<a href='#' class='Login-link'>Login</a></p>
                             </div>
                         </form>
                     </div>
@@ -34,5 +62,16 @@ function Form(){
         </div>
     )
 }
+
+const wrapper= document.querySelector('.wrapper');
+const loginlink=document.querySelector('.Login-link');
+const registerlink=document.querySelector('.register-link');
+registerlink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+
+loginlink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
 
 export default Form;
